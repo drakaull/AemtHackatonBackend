18411e8a668ccd00b5ac9f0273662241a9ce1879
drakaull <la238121@student.helha.be>
2026-01-19 09:25:44 +0100

    first commit

---
+++ src/main/java/org/helha/aemthackatonbackend/AemtHackatonBackendApplication.java
@@ -0,0 +1,13 @@
+package org.helha.aemthackatonbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AemtHackatonBackendApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(AemtHackatonBackendApplication.class, args);
+    }
+    
+}
