a9c6d052253b1bc3816ff9168819472d5be1f426
LouisB1210 <louisblampain@gmail.com>
2026-01-19 14:11:18 +0100

    Infrastructure V2

--- src/main/java/org/helha/aemthackatonbackend/infrastructure/folder/IFolderRepository.java
+++ src/main/java/org/helha/aemthackatonbackend/infrastructure/folder/IFolderRepository.java
@@ -6,5 +6,5 @@ import java.util.List;
 
 public interface IFolderRepository extends CrudRepository<DbFolder, Long> {
 
-    List<DbFolder> findByName(String name);
+  //  List<DbFolder> findByName(String name);
 }
--- src/main/java/org/helha/aemthackatonbackend/infrastructure/note/INoteRepository.java
+++ src/main/java/org/helha/aemthackatonbackend/infrastructure/note/INoteRepository.java
@@ -6,5 +6,5 @@ import java.util.List;
 
 public interface INoteRepository extends CrudRepository<DbNote, Long> {
 
-    List<DbNote> findByName(String name);
+//    List<DbNote> findByName(String name);
 }
