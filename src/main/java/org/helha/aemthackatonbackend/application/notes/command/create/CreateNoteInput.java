9f49fa18d79142b510e88670f690e23357857a27
LouisB1210 <louisblampain@gmail.com>
2026-01-20 10:13:44 +0100

    CreateNote functionnal

--- src/main/java/org/helha/aemthackatonbackend/application/notes/command/create/CreateNoteInput.java
+++ src/main/java/org/helha/aemthackatonbackend/application/notes/command/create/CreateNoteInput.java
@@ -1,14 +1,25 @@
+
 package org.helha.aemthackatonbackend.application.notes.command.create;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import lombok.Data;
 
+@Data
 public class CreateNoteInput {
 
     @NotBlank(message = "Le titre est obligatoire")
-    String title;
-    @NotNull(message = "L'identifiant est obligatoire")
+    private String title;
+
+    @NotNull(message = "L'identifiant du dossier est obligatoire")
     @Positive(message = "L'identifiant doit être positif")
-    Long folderId;
+    private Long folderId;
+
+    public CreateNoteInput(String title, Long folderId) {
+        this.title = title;
+        this.folderId = folderId;
+    }
+
+    public CreateNoteInput() {} // nécessaire pour @RequestBody
 }
