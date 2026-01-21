01ee3f4d0f418b1a9adcfde3e887a831b27077a9
drakaull <la238121@student.helha.be>
2026-01-21 12:23:50 +0100

    jdk 17 + downgrade maven version

--- src/test/java/org/helha/hallownote/integrations/folder/FolderControllerIT.java
+++ src/test/java/org/helha/hallownote/integrations/folder/FolderControllerIT.java
@@ -1,7 +1,17 @@
 package org.helha.hallownote.integrations.folder;
 
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) // Use the real database configuration
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD) // Reset context after each test
+@ActiveProfiles("test") // Use application-test.properties for testing
 public class FolderControllerIT {
 }
